e6646e1cb9f45109619a50ba4a44a74a59ecc059
vezype <vezype@gmail.com>
2021-11-07 14:19:16 +0300

    Начальная заставка + показ group boxов.

--- app.py
+++ app.py
@@ -14,6 +14,21 @@ class MainWindow(QMainWindow):
         uic.loadUi('interface/window.ui', self)
         self.setFixedSize(1080, 720)
 
+        self.group_boxes = {
+            self.btn_all: self.gb_all_tasks,
+            self.btn_today: None,
+            self.btn_upcoming: None,
+            self.btn_overdue: None,
+            self.btn_completed: None,
+            'Начальная заставка': None  # Показываем этот group box при запуске приложения.
+        }
+        # [group_box.hide() for group_box in list(self.group_boxes.values())]
+        [btn.clicked.connect(self.group_box_show) for btn in list(self.group_boxes.keys())[:-1]]
+        # self.group_boxes['Начальная заставка'].show()
+
+    def group_box_show(self) -> None:
+        self.group_boxes[self.sender()].show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
